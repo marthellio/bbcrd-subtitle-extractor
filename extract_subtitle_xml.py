78eb8d8da73655f9d349cee1d6318dc29f7ee893
marthellio <martha.elliott@bbc.co.uk>
2022-05-31 09:44:30 +0100

    initial code commit

--- extract_subtitle_xml.py
+++ extract_subtitle_xml.py
@@ -2,13 +2,21 @@ import requests
 import argparse
 import xml.etree.ElementTree as ET
 
+certificate_path = "/home/marthellio/.ssh/dev.bbc.co.uk.pem"
 
 def get_episode_data(episode_pid):
     api_for_episode_versions = "https://api.live.bbc.co.uk/pips/api/v1/episode/pid." + episode_pid + "/versions/"
     print("url = "+ api_for_episode_versions)
-    response = requests.get(api_for_episode_versions)
-    print("Status = "+response.status_code)
-    print("Content = "+response.content)
+    response = requests.get(
+        api_for_episode_versions,
+        #params=payload,    # optional (for query parameters)
+        #headers=HEADERS,   # optional
+        cert=certificate_path,
+        verify=False
+    )
+
+    print("Status = "+str(response.status_code))
+    print("Content = "+str(response.content))
 
 def parseOptions():
         parser = argparse.ArgumentParser(description='InputOptions')
